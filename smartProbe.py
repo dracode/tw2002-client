58b65a6f23162ac3d32837c07ea71d8e12518fc3
dracode <dracode@github.com>
2020-09-12 17:04:11 -0400

    Added commands to get sector warp reports for newly explored sectors after a Probe; faster than CIMing the whole universe

--- smartProbe.py
+++ smartProbe.py
@@ -336,5 +336,11 @@ if(__name__ == '__main__'):
         # plot the route again to verify that it follows our desired route
         print("F{}\n{}".format(route[0], route[-1]))
         print('')
+        print("After the probe is done, update your newly explored sectors:")
+        print("C")
+        for sector in route:
+            if(not sector in explored):
+                print("I{}".format(sector))
+        print('')
 
 
